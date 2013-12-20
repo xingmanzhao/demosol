33760c7261004cfc3e34756bf5ec4cb53309b3aa
Xingman Zhao <ext-xingman.zhao@nokia.com>
2013-12-20 09:50:13 +0800

    amend : add passage2 to master barnch

--- DemoWPF/MainWindow.xaml.cs
+++ DemoWPF/MainWindow.xaml.cs
@@ -620,6 +620,7 @@ namespace DemoWPF
             msgBuilder.Append("add msg3 on dev_e branch, ");
             msgBuilder.Append("add passage0 on master branch, ");
             msgBuilder.Append("add passage1 on master branch, ");
+            msgBuilder.Append("add passage2 on master branch, ");
             MessageBox.Show(msgBuilder.ToString());
         }
     }
